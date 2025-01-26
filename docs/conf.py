fbfcf6d76b115cd51feb7b4aab7a419e9a44b622
gfelber <34159565+gfelber@users.noreply.github.com>
2025-01-26 22:41:38 +0100

    updated docs year from 2024->2025

--- docs/conf.py
+++ docs/conf.py
@@ -10,36 +10,34 @@ import toml
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-pyproject = toml.load('../pyproject.toml') 
-project = pyproject['project']['name'].upper()
-copyright = '2024, 0x6fe1be2'
-author = pyproject['project']['authors'][0]['name']
-release = pyproject['project']['version']
+pyproject = toml.load("../pyproject.toml")
+project = pyproject["project"]["name"].upper()
+copyright = "2025, 0x6fe1be2"
+author = pyproject["project"]["authors"][0]["name"]
+release = pyproject["project"]["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-  'sphinx.ext.autodoc', 
-  'sphinx.ext.autosummary',
-  'sphinx.ext.napoleon', 
-  'sphinxcontrib.jquery',
-  'autoapi.extension',
+  "sphinx.ext.autodoc",
+  "sphinx.ext.autosummary",
+  "sphinx.ext.napoleon",
+  "sphinxcontrib.jquery",
+  "autoapi.extension",
 ]
 
-sys.path.insert(0, os.path.abspath('../src'))
-autoapi_dirs = ['../src/vagd']
-autoclass_content = 'both'
+sys.path.insert(0, os.path.abspath("../src"))
+autoapi_dirs = ["../src/vagd"]
+autoclass_content = "both"
 
-templates_path = ['_templates']
-autoapi_ignore = exclude_patterns = ['*env*', '*__pycache__*', '*.egg-info' , '*vagd/gdb/*']
+templates_path = ["_templates"]
+autoapi_ignore = exclude_patterns = ["*env*", "*__pycache__*", "*.egg-info", "*vagd/gdb/*"]
 autoapi_add_toctree_entry = False
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
